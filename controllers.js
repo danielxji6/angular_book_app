7e1b54e7793cbe6d54dc8cda2e1781ca118c476c
Daniel Lwo <danielxji6@gmail.com>
2016-02-10 17:29:35 -0800

    finish phase one

--- controllers.js
+++ controllers.js
@@ -12,19 +12,19 @@ function BooksController (Book) {
   function updateBook(book) {
     Book.update({id: book._id}, book);
     book.editForm = false;
-  };
+  }
 
   function createBook(){
     Book.save(this.newBook);
-    // TODO: clear the form!
-    // TODO: display the new book in the list of books!
-  };
+    this.books.push(this.newBook);
+    this.newBook = {};
+  }
 
   function deleteBook(book) {
     Book.remove({id:book._id});
     var bookIndex = this.books.indexOf(book);
     this.books.splice(bookIndex, 1);
-  };
+  }
 
   console.log("Controller loaded.");
-};
+}
